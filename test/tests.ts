23e5cfdd39d6e7388dcf3d6fbbde2415b068653b
James Quigley <jmquigley@outlook.com>
2017-03-18 10:40:07 -0500

    Reorganized dependencies
    - Removed the use of util.fixture.  It caused some circular dependency
    issue that caused NPM install to fail

--- test/tests.ts
+++ test/tests.ts
@@ -2,23 +2,12 @@
 
 import * as assert from 'assert';
 import * as fs from 'fs-extra';
-import {Fixture} from 'util.fixture';
 import {popd, pushd} from '../index';
 
 describe('Testing util.chdir', () => {
-
-	after(() => {
-		let directories = Fixture.cleanup();
-		directories.forEach((directory: string) => {
-			assert(!fs.existsSync(directory));
-		});
-	});
-
 	it('Validating directory change', () => {
-		let fixture = new Fixture();
-
 		let startingLocation: string = process.cwd();
-		let cwd = pushd(fixture.dir);
+		let cwd = pushd('test');
 		assert.equal(startingLocation, cwd);
 		assert(popd(), startingLocation);
 	});
