7d4bdbd6ae1c06d118e91bae5ba3556c50eee565
James Quigley <jmquigley@outlook.com>
2017-03-17 23:25:44 -0500

    Adds power assert to the module
    - Updated test cases to work with Fixture and typescript
    - Removed unneeded entries in gitignore and npmignore

--- test/tests.ts
+++ test/tests.ts
@@ -2,34 +2,23 @@
 
 import * as assert from 'assert';
 import * as fs from 'fs-extra';
-import * as path from 'path';
-import * as uuid from 'uuid';
+import {Fixture} from 'util.fixture';
 import {popd, pushd} from '../index';
-
-const home = require('expand-home-dir');
-
-let unitTestBaseDir = home(path.join('~/', '.tmp', 'unit-test-data'));
-let unitTestDir = home(path.join(unitTestBaseDir, uuid.v4()));
-if (fs.existsSync(unitTestDir)) {
-	fs.mkdirsSync(unitTestDir);
-}
 
 describe('Testing util.chdir', () => {
 
-	before(() => {
-		if (fs.existsSync(unitTestDir)) {
-			fs.removeSync(unitTestDir);
-		}
-		fs.mkdirsSync(unitTestDir);
-	});
-
 	after(() => {
-		fs.removeSync(unitTestBaseDir);
+		let directories = Fixture.cleanup();
+		directories.forEach((directory: string) => {
+			assert(!fs.existsSync(directory));
+		});
 	});
 
 	it('Validating directory change', () => {
+		let fixture = new Fixture();
+
 		let startingLocation: string = process.cwd();
-		let cwd = pushd(unitTestDir);
+		let cwd = pushd(fixture.dir);
 		assert.equal(startingLocation, cwd);
 		assert(popd(), startingLocation);
 	});
